1c9feb2778602dd0a6aa5900d657b347da65f2ce
zcj <405911774@qq.com>
2016-09-26 14:32:16 +0800

    Initial Commit

---
+++ ZCJCache/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZCJCache
+//
+//  Created by zhangchaojie on 16/9/26.
+//  Copyright © 2016年 zhangchaojie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZCJCache/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZCJCache
+//
+//  Created by zhangchaojie on 16/9/26.
+//  Copyright © 2016年 zhangchaojie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
